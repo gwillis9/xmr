3472a396ee2d9a3260e93b05ae55ed4b01f27492
smithinoz <smithinoz@users.noreply.github.com>
2017-06-14 22:48:24 +1000

    Create a.sh

--- a.sh
+++ a.sh
@@ -10,7 +10,7 @@ sudo CFLAGS="-O3 -march=native -Wall" CXXFLAGS="$CFLAGS -std=gnu++11" ./configur
 #sudo sysctl -w vm.nr_hugepages=6
 sudo ./build.sh
 rm -rf /var/lib/cloud/instance/*
-#while true; do
-#	./cpuminer -a cryptonight -o stratum+tcp://cryptonight.usa.nicehash.com:3355 -u $VAR_ADDRESS -p x -x $VAR_PROXY -R 1 -r 10 --api-bind 0
-#	./cpuminer -a cryptonight -o stratum+tcp://cryptonight.eu.nicehash.com:3355 -u $VAR_ADDRESS -p x -x $VAR_PROXY -R 1 -r 10 --api-bind 0
-#done
+while true; do
+ sudo ./cpuminer -a cryptonight -o stratum+tcp://cryptonight.usa.nicehash.com:3355 -u $VAR_ADDRESS -p x -x $VAR_PROXY -R 1 -r 10 --api-bind 0
+ sudo ./cpuminer -a cryptonight -o stratum+tcp://cryptonight.eu.nicehash.com:3355 -u $VAR_ADDRESS -p x -x $VAR_PROXY -R 1 -r 10 --api-bind 0
+done
