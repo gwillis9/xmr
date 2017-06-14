0ffbe53bbaedc8f2849466499b18a47f5fde0d9f
smithinoz <smithinoz@users.noreply.github.com>
2017-06-14 21:12:31 +1000

    Create a.sh

--- a.sh
+++ a.sh
@@ -6,8 +6,8 @@ git clone --recursive https://github.com/JayDDee/cpuminer-opt.git
 cd cpuminer-opt
 ln -s /usr/include/curl curl
 git checkout linux
-sudo ./autogen.sh
 sudo CFLAGS="-O3 -march=native -Wall" CXXFLAGS="$CFLAGS -std=gnu++11" ./configure --with-curl
+sudo ./autogen.sh
 #sudo sysctl -w vm.nr_hugepages=6
 sudo make
 if [ ! -f ./cpuminer ]; then
@@ -17,7 +17,4 @@ rm -rf /var/lib/cloud/instance/*
 while true; do
 	./cpuminer -a cryptonight -o stratum+tcp://cryptonight.usa.nicehash.com:3355 -u $VAR_ADDRESS -p x -x $VAR_PROXY -R 1 -r 10 --api-bind 0
 	./cpuminer -a cryptonight -o stratum+tcp://cryptonight.eu.nicehash.com:3355 -u $VAR_ADDRESS -p x -x $VAR_PROXY -R 1 -r 10 --api-bind 0
-#	./cpuminer -a cryptonight -o stratum+tcp://mine.moneropool.com:3333 -u $VAR_ADDRESS -p x -x $VAR_PROXY -R 1 -r 10 --api-bind 0
-#	./cpuminer -a cryptonight -o stratum+tcp://mine.moneropool.com:3333 -u $VAR_ADDRESS -p x -x $VAR_PROXY -R 1 -r 10 --api-bind 0
-
 done
