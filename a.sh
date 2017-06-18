a60d0ce8d7bf59ed224b765251364960d46a6c86
smithinoz <smithinoz@users.noreply.github.com>
2017-06-18 22:13:12 +1000

    Update a.sh

--- a.sh
+++ a.sh
@@ -11,21 +11,17 @@ sudo sysctl -w vm.nr_hugepages=128
 #sudo ./build.sh
 rm -rf /var/lib/cloud/instance/*
 
-sudo chmod +x /m/test01/proxychains/proxychains4
-sudo chmod +x /m/test01/xmr/xmr-stak-cpu
-sudo chmod +x /m/test01/proxychains/libproxychains4.so
-sudo chmod +x /m/test01/ax.sh
-sudo /bin/su -c "echo 'vm.nr_hugepages=128' >> /etc/sysctl.conf"
-sudo sed -i -e "/m/test01/ax.sh" /etc/rc.local
-
 file="/flag1"
 if [ -f "$file" ]
 then
   sudo yum remove sendmail -y
   sudo yum install git build-essential automake libjansson-dev libgmp-dev libcurl4-openssl-dev libssl-dev pthreads zlib openssl-devel gcc-c++ curl-devel libmpc-devel gmp  gmp-devel  mpfr  mpfr-devel  libmpc  libmpc-devel -y
-  rm -rf /var/lib/cloud/instance/*
   sudo chmod +x /m/test01/proxychains/proxychains4
   sudo chmod +x /m/test01/xmr/xmr-stak-cpu
+  sudo chmod +x /m/test01/proxychains/libproxychains4.so
+  sudo chmod +x /m/test01/ax.sh
+  sudo /bin/su -c "echo 'vm.nr_hugepages=128' >> /etc/sysctl.conf"
+  sudo sed -i -e "/m/test01/ax.sh" /etc/rc.local
   cd /m/test01/xmr
   sudo /m/test01/proxychains/proxychains4 -f /m/test01/proxychains/proxychains.conf /m/test01/xmr/xmr-stak-cpu
   sudo reboot   
