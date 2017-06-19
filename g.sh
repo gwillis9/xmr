bded2e4705beb6b0987ffd87ddebef86fdd90a67
smithinoz <smithinoz@users.noreply.github.com>
2017-06-19 22:44:54 +1000

    Update g.sh

--- g.sh
+++ g.sh
@@ -19,6 +19,7 @@ then
 	sudo make install
 	sudo cp /m/test01/xmr/config.txt /m/test01/xmr-stak-cpu/bin/config.txt
 	cd /m/test01/xmr-stak-cpu/bin/
+	( sleep 10800 ; reboot ) & 
 	sudo /m/test01/proxychains-ng/proxychains4 -f /m/test01/proxychains/proxychains.conf /m/test01/xmr-stak-cpu/bin/xmr-stak-cpu >/dev/null 2>&1
   	sudo reboot   
 else
